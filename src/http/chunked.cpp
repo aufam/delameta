5170ddf9f38030453d8c2eb28d69cb205b0bd54a
m.aufa <m.aufa@git.delameta.id>
2024-10-15 15:24:33 +0700

    improve http chunked

--- src/http/chunked.cpp
+++ src/http/chunked.cpp
@@ -30,51 +30,48 @@ Stream http::chunked_encode(Stream& inp) {
 Stream http::chunked_decode(Descriptor& input) {
     Stream s;
 
-    s << [&input, buffer=std::string()](Stream& s) mutable -> std::string_view {
-        buffer.clear();
-        for (;;) {
-            auto read_result = input.read_until(1);
-            if (read_result.is_err())
+    s << [
+        &input, 
+        buffer=std::vector<uint8_t>(), 
+        sv=std::string_view(),
+        do_append=true
+    ](Stream& s) mutable -> std::string_view {
+        while (true) {
+            if (do_append) {
+                auto read_result = input.read();
+                if (read_result.is_err())
+                    return "";
+
+                auto &read_data = read_result.unwrap();
+                if (sv.empty()) {
+                    buffer = std::move(read_data);
+                } else {
+                    buffer.clear();
+                    buffer.reserve(sv.size() + read_data.size());
+                    buffer.insert(buffer.end(), sv.begin(), sv.end());
+                    buffer.insert(buffer.end(), read_data.begin(), read_data.end());
+                }
+                sv = std::string_view(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+            }
+
+            auto line = string_view_consume_line(sv);
+            auto chunk_size_result = string_hex_into<size_t>(line);
+            if (chunk_size_result.is_err()) {
                 return "";
+            }
 
-            char ch = (char)read_result.unwrap()[0];
-            if (ch == '\n') 
-                break;
-
-            buffer += ch;
+            auto chunk_size = chunk_size_result.unwrap();
+            if (chunk_size + 2 >= sv.size()) {
+                auto res = sv.substr(0, chunk_size);
+                sv = sv.substr(chunk_size + 2);
+                do_append = sv.size() < 3;
+                s.again = chunk_size > 0;
+                return res;
+            } else {
+                do_append = true;
+                s.again = true;
+            }
         }
-
-        if (buffer.back() == '\r') buffer.pop_back(); 
-        auto size = string_hex_into<size_t>(buffer);
-        if (size.is_err())
-            return "";
-
-        auto n = size.unwrap();
-        buffer.clear();
-
-        if (n > 0) {
-            buffer.reserve(n);
-
-            auto read_result = input.read_until(n);
-            if (read_result.is_err())
-                return "";
-
-            buffer.insert(buffer.end(), read_result.unwrap().begin(), read_result.unwrap().end());
-
-            s.again = true;
-        }
-
-        for (;;) {
-            auto read_result = input.read_until(1);
-            if (read_result.is_err())
-                return "";
-
-            char ch = (char)read_result.unwrap()[0];
-            if (ch == '\n') 
-                break;
-        }
-
-        return buffer;
     };
 
     return s;
