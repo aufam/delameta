6747d0ecf1aa34470d9c2b27e28aab1571045ed2
m.aufa <m.aufa@git.delameta.id>
2024-10-23 14:00:52 +0700

    fix error http static for stm32

--- core/stm32_hal/http.cpp
+++ core/stm32_hal/http.cpp
@@ -1,8 +1,8 @@
 #include "delameta/http/http.h"
 
 using namespace Project;
-using namespace delameta;
 using etl::Err;
+namespace http = delameta::http;
 
 auto http::Http::Static(std::string path, const std::string& static_dir) -> delameta::Result<void> {
     return Err("Not implemented");
