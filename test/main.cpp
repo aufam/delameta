9e6734de17710fd2d686d8e04738247d51faadeb
m.aufa <m.aufa@git.delameta.id>
2024-12-18 17:10:38 +0700

    adding support for mingw

--- test/main.cpp
+++ test/main.cpp
@@ -7,3 +7,19 @@ int main(int argc, char **argv) {
     std::cout << (res == 0 ? "SUCCESS\n" : "FAILED\n");
     return res;
 }
+
+// mingw cannot see weak functions
+#ifdef _WIN32
+#include <delameta/debug.h>
+
+namespace Project::delameta {
+    void info(const char*, int, const std::string&) {}
+
+    void warning(const char*, int, const std::string&) {}
+
+    void panic(const char*, int, const std::string& msg) {
+        std::cerr << msg << '\n';
+        exit(1);
+    }
+}
+#endif
